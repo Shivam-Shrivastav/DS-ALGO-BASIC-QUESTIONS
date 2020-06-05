b61c33e0bf2a7cb827e14a243681a6bfffd2b0aa
s H I v A M <62115066+starboy2102@users.noreply.github.com>
2020-06-05 21:02:53 +0530

    Update infix-to-postfix.cpp

--- Stack/Infix-To-Postfix-Using-Stack/infix-to-postfix.cpp
+++ Stack/Infix-To-Postfix-Using-Stack/infix-to-postfix.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int prec(char c)
+int prec(char c)//Giving precedence to operators.
 {
     if (c =='^')
     {
@@ -23,25 +23,25 @@ int prec(char c)
 
 }
 
-void infixToPostfix(string exp)
+void infixToPostfix(string exp)//Function to change infix to postfix
 {
     std::stack<char> st;
-    st.push('N');
-    int l = exp.length();
+    st.push('N');//Bottom of the stack, will be helpful later to assuming stack as empty.
+    int l = exp.length();//Length of the expression.
     string ns;
     for (int i = 0; i < l; i++)
     {
         if (exp[i]>='a' && exp[i]<='z' || exp[i]>='A' && exp[i]<='Z')
         {
-            ns += exp[i];
+            ns += exp[i];//Insert variable to form postfix expression.
         }
         else if (exp[i]== '(')
         {
-            st.push('(');
+            st.push('(');//Push in stack
         }
         else if (exp[i]== ')')
         {
-            while (st.top()!='N' && st.top()!='(')
+            while (st.top()!='N' && st.top()!='(')//Unless stack got empty or '(' parantheses come
             {
                 char c = st.top();
                 st.pop();
@@ -55,6 +55,7 @@ void infixToPostfix(string exp)
         }
         else
         {
+            //Unless stack got empty and scanner opererator precedence should be lower than the operator in stack.
             while (st.top()!='N' && prec(exp[i])<=prec(st.top()))
             {
                 char c = st.top();
@@ -80,6 +81,6 @@ void infixToPostfix(string exp)
 int main(int argc, char const *argv[])
 {
     string exp ="a+b*(c^d-e)^(f+g*h)-i";
-    infixToPostfix(exp);
+    infixToPostfix(exp);//Calling function.
     return 0;
 }
